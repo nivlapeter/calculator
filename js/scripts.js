37bc35d1689adf0900760ee80b010fb1d5dc7d48
Alvin Peter <nivlapeter.0549@gmail.com>
2019-03-19 12:07:47 +0300

    set division

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
-var subtract = function(number1, number2){
-    return number1 - number2;
+var divide= function(number1, number2){
+    return number1 / number2;
     }
     var number1 = parseInt(prompt("enter the number"));
     var number1 = parseInt(prompt("enter the other number"));
-    alert(subtract (number1, number2));
+    alert(divide (number1, number2));
     
