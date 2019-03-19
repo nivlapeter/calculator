1a26f138737df9faeda3c93913dea78e69bf0bf7
Alvin Peter <nivlapeter.0549@gmail.com>
2019-03-19 12:01:31 +0300

    add project to github repository

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var add = function(number1, number2){
+    return number1 + number2;
+    }
+    var number1 = parseInt(prompt("enter the number"));
+    var number1 = parseInt(prompt("enter the other number"));
+    alert(add (number1, number2));
+    
