7254ba719abe09cc3e420df4a31978399c433798
Alvin Peter <nivlapeter.0549@gmail.com>
2019-03-19 12:09:26 +0300

    set multiplication

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
-var divide= function(number1, number2){
+var multiply= function(number1, number2){
     return number1 / number2;
     }
     var number1 = parseInt(prompt("enter the number"));
     var number1 = parseInt(prompt("enter the other number"));
-    alert(divide (number1, number2));
+    alert(multiply (number1, number2));
     
