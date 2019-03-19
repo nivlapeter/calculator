5f8d4004f7d488364ebf499b1e46b6c16e54d957
Alvin Peter <nivlapeter.0549@gmail.com>
2019-03-19 12:05:59 +0300

    set subtraction

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
-var add = function(number1, number2){
-    return number1 + number2;
+var subtract = function(number1, number2){
+    return number1 - number2;
     }
     var number1 = parseInt(prompt("enter the number"));
     var number1 = parseInt(prompt("enter the other number"));
-    alert(add (number1, number2));
+    alert(subtract (number1, number2));
     
